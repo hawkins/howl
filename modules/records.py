b4be2fc4817de7260f55ee0f06ec6b3f5f414a7a
Josh Hawkins <jkhaccounts@gmail.com>
2016-01-01 21:49:18 -0600

    Updated records.py

--- modules/records.py
+++ modules/records.py
@@ -3,12 +3,14 @@ import argparse
 import sys
 import os
 import time
+from timeout import timeout
 import logging
+from pyvirtualdisplay import Display
 import csv
 from logging.handlers import TimedRotatingFileHandler
 import json
 import math
-import selenium.common.exceptions
+from selenium.common.exceptions import *
 import initium
 import string
 import re
@@ -39,24 +41,24 @@ class Records(initium.webdriver, initium.initium):
 
         logger.info("Initializing webdriver")
         # Hax
-        #super().__init__()
+        super().__init__()
 
         logger.info("Connecting to Initium")
-        #self.get("http://www.playinitium.com")
+        self.get("http://www.playinitium.com")
 
         logger.info("Logging in as {0}".format(self.cfg["uname"]))
         # Enter email
-        #login = self.find_element_by_name("email")
-        #login.send_keys(self.cfg["email"])
+        login = self.find_element_by_name("email")
+        login.send_keys(self.cfg["email"])
 
         # Enter pw
-        #login = self.find_element_by_name("password")
-        #login.send_keys(self.cfg["pw"])
+        login = self.find_element_by_name("password")
+        login.send_keys(self.cfg["pw"])
 
-        #for button in self.find_elements_by_class_name(
-        #        "main-button"):
-        #    if button.text == "Login":
-        #        button.click()
+        for button in self.find_elements_by_class_name(
+               "main-button"):
+           if button.text == "Login":
+               button.click()
 
     def _parse_config(self):
         logger.info('Parsing command line arguments')
@@ -82,10 +84,33 @@ class Records(initium.webdriver, initium.initium):
             logger.error("Error: Config: {0} not found".format(self.args.config_file))
             sys.exit(1)
 
-    def calculate_score_weapon(self, dice, size, chance, mult):
-        ## Calculates a weapon's score based on
-        ## 4D13, 07.77% chance for 3x damage = (4*13) * 3 * (2^1.0777)
-        score = dice + 0.4 * math.pow((dice * size), 1.5) * (mult / 4.0) * math.pow(2.2, (1.0+(chance*3/100.0)))
+    # @timeout(60)
+    def calculate_score_weapon(self=0, dice=0, sides=0, chance=0, mult=0):
+        ## Calculates a weapon's score based on damaeg and critical stats
+        # Ensure the popup loads but we don't wait too long
+        start = time.time()
+        DEF_MAX_RUNTIME = 14.9
+        loaded = False
+        while not loaded:
+            try:
+                end = time.time()
+                if end-start > DEF_MAX_RUNTIME:
+                    logger.warning("Score calculation timed out!")
+                    return -1
+                elements = self.find_elements_by_class_name("main-item-subnote")
+                damage = elements[0].get_attribute("innerHTML").split("D")
+                try:
+                    dice = int(damage[0])
+                except ValueError:
+                    logger.warning("Inspected armor item!")
+                    return -1
+                sides = int(damage[1])
+                chance = float(elements[1].get_attribute("innerHTML").split("%")[0])
+                mult = float(elements[2].get_attribute("innerHTML").split("x")[0])
+                loaded = True
+            except:
+                pass
+        score = dice + 0.4 * math.pow((dice * sides), 1.5) * (mult / 4.0) * math.pow(2.2, (1.0+(chance*3/100.0)))
         return score
 
     def save_records(self, item_name, item_id, item_score):
@@ -94,6 +119,7 @@ class Records(initium.webdriver, initium.initium):
         # Variables for scope
         item_records = []
         records_file = None
+        result = -1
         ## Open the file
         # First check if file exists
         path = "../logs/records/items/"+item_name+".txt"
@@ -109,8 +135,7 @@ class Records(initium.webdriver, initium.initium):
         for each in item_records:
             if str(each[1]) == str(item_id):
                 # ID is found in records already
-                logger.info("Item is already in records, no need to save.")
-                return
+                return result
 
         ## Now open file for writing
         records_file = open(path, 'w')
@@ -118,11 +143,13 @@ class Records(initium.webdriver, initium.initium):
         ## Now append new item
         if len(item_records) == 0:
             item_records.append([item_score, item_id])
+            result = 0
         else:
             done = False
             for each in item_records:
                 if item_score > float(each[0]):
-                    item_records.insert(item_records.index(each), [item_score, item_id])
+                    result = item_records.index(each) # Where we place the item info
+                    item_records.insert(result, [item_score, item_id])
                     done = True
                     break
             if not done:
@@ -133,7 +160,7 @@ class Records(initium.webdriver, initium.initium):
         ## Finish: close file and log success
         records_file.close()
         logger.info("Successfully saved item")
-        return
+        return result
 
 
 
@@ -144,40 +171,46 @@ if __name__ == "__main__":
     logger = create_timed_rotating_log(main_log_file)
 
     Bot = Records()
-    #logger.info("Current Location: " + Bot.get_location())
-    logger.info("Now listening for requests")
-
-    # Debug
-    # Now try this
-    score = Bot.calculate_score_weapon(2, 8, 10, 4)
-    print(score)
-    Bot.save_records("longsword", 666777888, score)
-    score = Bot.calculate_score_weapon(2, 8, 20, 2)
-    print(score)
-    Bot.save_records("longsword", 567678789, score)
+    logger.info("Now listening for item shares")
 
     while True:
+        ## Look in Global chat
+        Bot.update_messages("Global")
 
-        # Check location tab for "hoot" or " owl" in last message
-        hootauth, hootmsg = Owl.update_messages("Location")
-        for each in hootmsg[0:4]:
-            if "Hoot hoot! I am Owl. PM me to search shops!" not in each:
-                if "hoot" in each.lower():
-                    Owl.say("Location", "Hoot hoot! I am Owl. PM me to search shops!")
-                    logger.info("Completed hoot request!")
-                    break
-                if " owl" in each.lower():
-                    Owl.say("Location", "Hoot hoot! I am Owl. PM me to search shops!")
-                    logger.info("Completed hoot request!")
-                    break
-            else:
+        ## Check for item shares
+        items = Bot.find_elements_by_class_name("chat-embedded-item")
+        # Iterate through each, saving them all
+        for each in items:
+            try:
+                # Click the item
+                link = each.find_elements_by_tag_name("a")[0]
+                # for some reason, sometimes this winds up being a share button.
+                # so for now, DO NOT CLICK if it says share
+                if link.get_attribute("innerHTML") != "Share":
+                    link.click()
+            except ElementNotVisibleException:
+                logger.warning("Item not visible, updating messages instead.")
                 break
 
-        # Rebuild list of messages
-        authors, texts = [], []
-        while (len(authors) == 0):
-            authors, texts = Owl.update_messages("Global")
-            if (len(authors[0]) == 0):
-                authors, queries = [], []
+            # Calculate score of popup item
+            score = Bot.calculate_score_weapon()
+            if score == -1:
+                # Inspecetd item was not a weapon, so skip to next item
+                continue
+            # Get item ID from "rel="viewitemmini.jsp?itemId=4961250779856896"
+            itemID = link.get_attribute("rel").split("itemId=")[1]
+            # Get the item name
+            original_item_name = ' '.join(link.find_elements_by_tag_name("div")[1].get_attribute("innerHTML").split())
+            item_name = ''.join(link.find_elements_by_tag_name("div")[1].get_attribute("innerHTML").split())
+            item_name = re.sub('(?!\s)[\W_]', '', item_name) # Need to also strip spaces
+            # Save the item
+            result = Bot.save_records(item_name, itemID, score)
+            if result != -1:
+                logger.info("Saved " + original_item_name + " in resulting index " +str(result))
+            if result == 0:
+                # Best of its kind
+                author_element = link.find_elements_by_xpath('../../..')[0].find_elements_by_tag_name("span")[1].find_elements_by_tag_name("a")[0]
+                author = author_element.get_attribute("innerHTML")
+                Bot.say("Global", author+" has just discovered the best "+original_item_name+", with a score of "+"{:10.2f}".format(score) + "!" )
 
     logger.error("OUTSIDE WHILE TRUE LOOP!")
